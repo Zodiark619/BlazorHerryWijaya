c0331e23e46c607dcb56e7f929dd022b42e029d2
asd asd <seonghyejin33@gmail.com>
2025-09-30 09:18:19 +0700

    add json download

--- BlazorHerryWijaya/wwwroot/js/common.js
+++ BlazorHerryWijaya/wwwroot/js/common.js
@@ -15,3 +15,9 @@ window.DataTableInterop = {
     //    }
     //}
 };
+function downloadFileFromUrl(fileName, url) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    link.click();
+}
